e7554c36cd5bcdf31eb10a5508fa8c0a66b5c9a4
RTanSan <quasartan030@gmail.com>
2023-06-05 14:23:48 -0700

    4. strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes.
+ * @s: qwert
+ * @accept: wertyu
+ *
+ * Return: qwedrf
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	size_t i = 0;
+	size_t n = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (n = 0; accept[n] != '\0'; n++)
+		{
+			if (s[i] == accept[n])
+			{
+				return &s[i];
+			}
+
+		}
+	}
+
+	return (0);
+}
