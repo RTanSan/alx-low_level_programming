c8b86f7aa480f895bb9a3d4fab16598ac323b5e3
RTanSan <quasartan030@gmail.com>
2023-06-19 15:32:28 -0700

    0x0E. C - Structures, typedef: For more information on structers and typedef check file

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,13 +18,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (new_dog == NULL)
 	{
-		return(NULL);
+		return (NULL);
+		printf("\n");
 	}
 
 
-	new_dog -> name = name;
-	new_dog -> age = age;
-	new_dog -> owner = owner;
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
 
 	return (new_dog);
 }
