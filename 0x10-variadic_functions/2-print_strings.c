385ce0f713d16cbb70587026a68e40e90ae5ea56
RTanSan <quasartan030@gmail.com>
2023-06-22 18:14:57 -0700

    Variadic functions

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_strings - prints strings
+ * @separator: string to be printed between the strings
+ * @n: number of strings passed to the function
+ *
+ * Return:
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	if 
