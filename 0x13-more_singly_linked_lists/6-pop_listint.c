32049c4b6cd1059ab9a991c4b517fc22003699af
RTanSan <quasartan030@gmail.com>
2023-07-04 19:20:11 -0700

    a function that returns the nth node of a listint_t linked list.

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of a listint_t linked list
+ * @head: //
+ * Description: if the linked list is empty return 0
+ *
+ * Return: returns the head node’s data (n).
+ */
+int pop_listint(listint_t **head)
+{
+	int n = 0;
+	listint_t *node = 0;
+
+	if (head == NULL || *head == NULL)
+	{
+		return(0);
+	}
+
+	node = *head;
+	*head = (*head)->next;
+	n = node->n;
+	free(node);
+
+	return (n);
+}
