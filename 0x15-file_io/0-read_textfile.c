71f7f5c49577af22957873214cebd4ef895964a0
RTanSan <quasartan030@gmail.com>
2023-07-14 00:33:07 -0700

    Prototype: ssize_t read_textfile(const char *filename, size_t letters); and int create_file(const char *filename, char *text_content);

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,11 +3,12 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * read_textfile - reads a text file and
+ * prints it to the POSIX standard output.
  * @filename: //
  * @letters: //
  * Description: where letters is the number of letters it should read and print
- * 
+ *
  * Return: returns the actual number of letters it could read and print
  * if the file can not be opened or read, return 0
  * if filename is NULL return 0
@@ -21,13 +22,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 	{
-		return 0;
+		return (0);
 	}
 
 	textfile = fopen(filename, "r");
 	if (textfile == NULL)
 	{
-		return 0;
+		return (0);
 	}
 
 	for (; i < letters; i++)
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 	{
-		return(-1);
+		return (-1);
 	}
 
 	fp = fopen(filename, "w");
