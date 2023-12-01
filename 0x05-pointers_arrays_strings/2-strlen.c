b05d26b2c3429128d24d180aaee442239ca75e82
RTanSan <quasartan030@gmail.com>
2023-12-01 09:27:41 -0800

    a function that takes a pointer to an int as parameter and updates the value it points to to 98. and returns the length of a string.

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: length of a string.
+ *
+ * Return: success (0)
+ */
+int _strlen(char *s)
+{
+	return (*s);
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+C - Pointers, arrays and strings
+Tasks
+
+a function that takes a pointer to an int as parameter and updates the value it points to to 98.
