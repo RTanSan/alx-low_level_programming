7d7ee942dd485e2b747309ee34b5dae763760e82
RTanSan <quasartan030@gmail.com>
2024-03-09 05:55:31 -0800

    C program that prints and that piece of art is.... followed by a new line, to the standard error.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * main - C program to print expected char
  * Return: return 1
  */
-int main()
+int main(void)
 {
 	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
