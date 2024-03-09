c02cb4f42f92f9c8211ae416b2722cd750d86f96
RTanSan <quasartan030@gmail.com>
2024-03-09 05:49:57 -0800

    Tryout

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ * main - C program to print expected char
+ * Return: return 1
+ */
 int main()
 {
 	puts("\"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
